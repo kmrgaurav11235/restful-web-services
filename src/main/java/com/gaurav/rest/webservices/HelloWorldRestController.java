88763a3328da347cb3f45479bef3ad32cd2e14d3
Kumar Gaurav <kgaurav@adobe.com>
2019-12-14 14:07:42 +0530

    Return a bean instead of String

--- src/main/java/com/gaurav/rest/webservices/HelloWorldRestController.java
+++ src/main/java/com/gaurav/rest/webservices/HelloWorldRestController.java
@@ -9,4 +9,9 @@ public class HelloWorldRestController {
 	public String helloWorld() {
 		return "Hello World";
 	}
+	
+	@GetMapping("/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
 }
