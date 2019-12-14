571f9672031e0812fa4fd1c63460054ebe050c56
Kumar Gaurav <kgaurav@adobe.com>
2019-12-14 16:19:19 +0530

    Implement POST method for User resource

--- src/main/java/com/gaurav/rest/webservices/user/UserResource.java
+++ src/main/java/com/gaurav/rest/webservices/user/UserResource.java
@@ -1,11 +1,17 @@
 package com.gaurav.rest.webservices.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.BodyBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -21,5 +27,21 @@ public class UserResource {
 	@GetMapping("/users/{id}")
 	public User retrieveOneUser(@PathVariable int id) {
 		return userDaoService.findOne(id);
+	}
+	
+	@PostMapping("/users")
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
+		User savedUser = userDaoService.save(user);
+		
+		// Create the URL of new resource so that it can be returned back
+		URI location = ServletUriComponentsBuilder
+			.fromCurrentRequestUri() // Gets the Current Request URI. Now, we can modify it to get resource URI
+			.path("/{id}") // Append the given path to the existing path of this builder.
+			.buildAndExpand(savedUser.getId()) // Build a UriComponents instance and replaces URI template variables with the value
+			.toUri();
+		
+		return ResponseEntity.created(location)
+				.build(); // Create a new builder with a CREATED http status and a location header set to the given URI.
+
 	}
 }
