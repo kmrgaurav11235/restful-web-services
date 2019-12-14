f09be72dbadb2a5abede7ff6922c4cbb77294516
Kumar Gaurav <kgaurav@adobe.com>
2019-12-14 14:35:01 +0530

    Add a path variale

--- src/main/java/com/gaurav/rest/webservices/HelloWorldRestController.java
+++ src/main/java/com/gaurav/rest/webservices/HelloWorldRestController.java
@@ -1,6 +1,7 @@
 package com.gaurav.rest.webservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +14,10 @@ public class HelloWorldRestController {
 	@GetMapping("/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World");
+	}
+	
+	@GetMapping("/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello %s", name));
 	}
 }
